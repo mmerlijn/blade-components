f1c337c22100e348c3a1d505821fb92c5f1731c8
Menno <menno@mmerlijn.nl>
2021-05-07 14:51:55 +0200

    add badge

--- src/helpers/HasTheme.php
+++ src/helpers/HasTheme.php
@@ -8,6 +8,6 @@ trait HasTheme
 
     public function getThemeClasses()
     {
-        return config("blade-components.components.".__CLASS__.".themes.".$this->theme)?:'name:'.$this->componentName;
+        return config("blade-components.components.".__CLASS__.".themes.".$this->theme)?:'name:'.__CLASS__;
     }
 }
