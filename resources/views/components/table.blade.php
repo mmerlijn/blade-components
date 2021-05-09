97f009a9799e9310a034daada38bece73beeacfe
Menno <menno@mmerlijn.nl>
2021-05-09 13:27:54 +0200

    components added

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -4,7 +4,9 @@
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
+                    <tr>
                     {{$header}}
+                    </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
                     {{$slot}}
