47820d44aa9ea6bc9bc62a0a48e1b082fd4e2cda
Menno <menno@mmerlijn.nl>
2021-05-09 14:21:22 +0200

    components added

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,2 +1,4 @@
 @props(['name'=>false,'value'])
-<label @if($name)for="{{$name??''}}" @endif {{ $attributes->merge(['class' => 'block text-sm font-medium text-gray-700'])}}>{{$value}}</label>
+<label @if($name)for="{{$name??''}}" @endif {{ $attributes->merge(['class' => 'block text-sm font-medium text-gray-700'])}}>
+    {{$value}}
+</label>
