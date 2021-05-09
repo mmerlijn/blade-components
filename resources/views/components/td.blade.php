ba8c9a5d60b7105974e8f4ef23b35147d2cab03c
Menno <menno@mmerlijn.nl>
2021-05-09 13:37:56 +0200

    fix

--- resources/views/components/td.blade.php
+++ resources/views/components/td.blade.php
@@ -1,3 +1,4 @@
-<td {{$attributes->merge(['class'=> "px-6 py-4 whitespace-nowrap" ])}}>
+@props(['theme'=>'default'])
+<td {{$attributes->merge(['class'=> "px-6 py-4 whitespace-nowrap ".\mmerlijn\bladeComponents\helpers\BladeTheme::getThemeText($theme) ])}}>
     {{$slot}}
 </td>
