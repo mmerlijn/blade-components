13b7503a54e93072f4d0b5544ccd60801cf1fb62
Menno <menno@mmerlijn.nl>
2021-05-09 13:36:16 +0200

    fix

--- resources/views/components/th.blade.php
+++ resources/views/components/th.blade.php
@@ -1,3 +1,4 @@
-<th scope="col" {{$attributes->merge(['class'=> "px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" ])}}>
+@props(['theme'=>'default'])
+<th scope="col" {{$attributes->merge(['class'=> "px-6 py-3 text-left text-xs font-medium uppercase tracking-wider ".\mmerlijn\bladeComponents\helpers\BladeTheme::getThemeText($theme) ])}}>
     {{$slot}}
 </th>
