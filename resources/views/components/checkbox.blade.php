8745562d27608612aa802581d75cd020b3a55aee
Menno <menno@mmerlijn.nl>
2021-05-09 14:05:25 +0200

    components added

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,3 +1,3 @@
 @props(['name','label'=>''])
 <input name="{{$name}}" id="{{$name}}" type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
-<bladeComponents::components.label for="{{$name}}">{{$label}}</bladeComponents::components.label>
+<label class="ml-3 block text-sm font-medium text-gray-700" for="{{$name}}">{{$label}}</label>
