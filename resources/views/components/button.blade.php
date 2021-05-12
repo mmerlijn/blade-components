7c5e74ecf9753d738f51ad37aab885e58dc10fbf
Menno <menno@mmerlijn.nl>
2021-05-12 21:36:12 +0200

    button fix

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,10 @@
 @props(['color'=>'gray','href'=>''])
-
-<button {{$href?"href='$href'":"type='submit'"}} {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-'.$color.'-600 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-'.$color.'-500 active:bg-'.$color.'-800 focus:outline-none focus:border-'.$color.'-900 focus:ring focus:ring-'.$color.'-200 disabled:opacity-25 transition']) }}>
-    {{ $slot }}
-</button>
+@if($href)
+    <a href="{{$href}}">
+        @endif
+        <button {{($href)?"":"type='submit'"}} {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-'.$color.'-600 border border-transparent rounded-md text-white tracking-widest hover:bg-'.$color.'-500 active:bg-'.$color.'-800 focus:outline-none focus:border-'.$color.'-900 focus:ring focus:ring-'.$color.'-200 disabled:opacity-25 transition']) }}>
+            {{ $slot }}
+        </button>
+        @if($href)
+    </a>
+@endif
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
     $classes = ($active ?? false)
-                ? 'inline-flex items-center px-1 pt-1 border-b-2 border-'.$color.'-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-'.$color.'-700 transition'
-                : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition';
+                ? 'inline-flex items-center px-1 pt-1 border-b-2 border-'.$color.'-400 font-medium leading-5 text-'.$color.'-900 focus:outline-none focus:border-'.$color.'-700 transition'
+                : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent font-medium leading-5 text-'.$color.'-500 hover:text-'.$color.'-700 hover:border-'.$color.'-300 focus:outline-none focus:text-'.$color.'-700 focus:border-'.$color.'-300 transition';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
