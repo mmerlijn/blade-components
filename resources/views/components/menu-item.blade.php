ed838cb56851de85186465570967fe622a18e2e5
Menno <menno@mmerlijn.nl>
2021-05-12 14:43:11 +0200

    menu toegevoegd

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,2 +1,13 @@
-<a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-  role="menuitem">{{$slot}}</a>
+@props(['post'=>false])
+@if($post)
+    <form method="POST" {{$attributes}} role="none">
+        <button type="submit"
+                class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                role="menuitem">
+            Sign out
+        </button>
+    </form>
+@else
+    <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+       role="menuitem" {{$attributes}}>{{$slot}}</a>
+@endif
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,14 +1,7 @@
 @props(['open'=>false])
-<div
-        {{$open?'open':''}}
-        x-data
-        x-ref="dropdown"
-        @click.away="$refs.dropdown.removeAttribute('open');"
-        class="relative inline-block text-left"
->
+<div {{$open?'open':''}} x-data x-ref="dropdown" @click.away="$refs.dropdown.removeAttribute('open');" class="relative inline-block text-left">
     <div>
-        <div class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-        >
+        <div class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
             Options
             <!-- Heroicon name: solid/chevron-down -->
             <svg
@@ -27,31 +20,12 @@
         </div>
     </div>
 
-    <div role="menu"
-         class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+    <div role="menu" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
         <x-bc-menu-group>
             <x-bc-menu-item href="#">Account settings</x-bc-menu-item>
-            <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                    role="menuitem"
-            >Support</a
-            >
-            <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                    role="menuitem"
-            >License</a
-            >
-            <form method="POST" action="#" role="none">
-                <button
-                        type="submit"
-                        class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                        role="menuitem"
-                >
-                    Sign out
-                </button>
-            </form>
+            <x-bc-menu-item href="#">Support</x-bc-menu-item>
+            <x-bc-menu-item href="#">Licence</x-bc-menu-item>
+            <x-bc-menu-item post="true" action="#">Sign out</x-bc-menu-item>
         </x-bc-menu-group>
         <x-bc-menu-group>
             <x-bc-menu-item href="#">Account settings</x-bc-menu-item>
