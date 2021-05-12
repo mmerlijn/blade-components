d52dec5c9ed491b379454e679b4a64dd0df5321e
Menno <menno@mmerlijn.nl>
2021-05-12 14:29:15 +0200

    menu toegevoegd

---
+++ resources/views/components/menu-item.blade.php
@@ -0,0 +1,2 @@
+<a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+  role="menuitem">{{$slot}}</a>
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,13 +1,13 @@
-<details
-        open
+@props(['open'=>false])
+<div
+        {{$open?'open':''}}
         x-data
         x-ref="dropdown"
         @click.away="$refs.dropdown.removeAttribute('open');"
         class="relative inline-block text-left"
 >
-    <summary>
-        <div
-                class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+    <div>
+        <div class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
         >
             Options
             <!-- Heroicon name: solid/chevron-down -->
@@ -25,19 +25,12 @@
                 />
             </svg>
         </div>
-    </summary>
+    </div>
 
-    <details-menu
-            role="menu"
-            class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-    >
-        <div class="py-1" role="none">
-            <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                    role="menuitem"
-            >Account settings</a
-            >
+    <div role="menu"
+         class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <x-bladeComponents::menu-group>
+            <x-bladeComponents:x-bladeComponents::menu-item href="#">Account settings</x-bladeComponents:x-bladeComponents::menu-item>
             <a
                     href="#"
                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
@@ -59,6 +52,9 @@
                     Sign out
                 </button>
             </form>
-        </div>
-    </details-menu>
-</details>
+        </x-bladeComponents::menu-group>
+        <x-bladeComponents::menu-group>
+            <x-bladeComponents:x-bladeComponents::menu-item href="#">Account settings</x-bladeComponents:x-bladeComponents::menu-item>
+        </x-bladeComponents::menu-group>
+    </div>
+</div>
