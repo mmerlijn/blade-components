01428668ce47bfab501e7811f6afabceeb47f62a
Menno <menno@mmerlijn.nl>
2021-05-12 14:53:41 +0200

    menu toegevoegd

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -4,7 +4,7 @@
         <button type="submit"
                 class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                 role="menuitem">
-            Sign out
+            {{$slot}}
         </button>
     </form>
 @else
