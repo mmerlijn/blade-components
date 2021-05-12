ff3f4b1945367dc26816a0718eab39509c4d0f10
Menno <menno@mmerlijn.nl>
2021-05-12 20:00:41 +0200

    fix menu-bar

---
+++ resources/views/components/menu-bar.blade.php
@@ -0,0 +1,26 @@
+@props(['logo'=>'','right'=>''])
+<nav>
+    <div class="mx-auto">
+        <div class="flex items-center justify-between h-12">
+            <div class="flex items-center">
+                @if($logo)
+                    <div class="flex-shrink-0">
+                        {{$logo}}
+                    </div>
+                @endif
+                <div class="block">
+                    <div class="ml-10 flex items-baseline space-x-4">
+                        {{$slot}}
+                    </div>
+                </div>
+            </div>
+            @if($right)
+                <div class="block">
+                    <div class="ml-4 flex items-center md:ml-6">
+                        {{$right}}
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
+</nav>
--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,14 +1,13 @@
 @props(['post'=>false])
 @if($post)
-    <form method="POST" {{$attributes}} >
+    <form method="post" {{$attributes}} >
         @csrf
         <button type="submit"
-                class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                role="menuitem">
+                class="inline-block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
             {{$slot}}
         </button>
     </form>
 @else
-    <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+    <a class="inline-block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
        {{$attributes}}>{{$slot}}</a>
 @endif
