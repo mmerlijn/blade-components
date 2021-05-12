0eef88900dbb1eacc07198cc3b892880aa2f2276
Menno <menno@mmerlijn.nl>
2021-05-12 20:04:46 +0200

    logo salt added

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -3,11 +3,11 @@
     <form method="post" {{$attributes}} >
         @csrf
         <button type="submit"
-                class="inline-block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+                class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
             {{$slot}}
         </button>
     </form>
 @else
-    <a class="inline-block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+    <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
        {{$attributes}}>{{$slot}}</a>
 @endif
